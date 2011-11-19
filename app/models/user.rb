b58dd7ccaa91d1eec3bf1005979d77ef690ca8d3
jlytle <jonathanlytle@hotmail.com>
2011-11-19 10:54:55 -0700

    Finished section 7.1 and moving on to section 7.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,18 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
+
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -19,5 +22,25 @@ class User < ActiveRecord::Base
   validates :email, :presence   => true,
                     :format     => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
+                    
+  # Automatically create the virtual attribute 'password_confirmation'.
+  validates :password,  :presence     => true,
+                        :confirmation => true,
+                        :length       => { :within => 6..40 }
+                        
+  before_save :encrypt_password
+  
+  private
+  
+    def encrypt_password
+      self.encrypted_password = encrypt(password)
+    end
+    
+    def encrypt(string)
+      string # Only a temporary implementation!
+    end
+                        
 
 end
+
+
