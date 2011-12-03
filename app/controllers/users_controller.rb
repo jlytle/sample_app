5d040bc30d7b7a68568dae9024e03f9b17b8aad5
jlytle <jonathanlytle@hotmail.com>
2011-12-02 19:38:07 -0700

    Finished 10.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
   
   def show
-    @user = User.find(params[:id])
+    @user  = User.find(params[:id])
     @title = @user.name
   end
     
   def new
-    @user = User.new
+    @user  = User.new
     @title = "Sign up"
   end
   
@@ -22,6 +22,22 @@ class UsersController < ApplicationController
       @user.password.clear
       @user.password_confirmation = nil
       render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+  
+  def update
+    @user = User.find(params["id"])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
     end
   end
 end
