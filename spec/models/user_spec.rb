9b6e219856e291c5a7ee6348b73d5ff136a0f98a
jlytle <jonathanlytle@hotmail.com>
2011-12-17 21:09:44 -0700

    Added user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -154,41 +154,50 @@ describe User do
   describe "micropost associations" do
     
     before(:each) do
-      @user = User.create(@attr)
-      @mp1 = Factory(:micropost, :user => @user, :created_at => 1.day.ago)
-      @mp2 = Factory(:micropost, :user => @user, :created_at => 1.hour.ago)
-    end
-    
-    it "should have a microposts attribute" do 
-      @user.should respond_to(:microposts)
-    end
-    
-    it "should have the right microposts in the right order" do
-      @user.microposts.should == [@mp2, @mp1]
-    end
-    
-    it "should destroy associated microposts" do
-      @user.destroy
-      [@mp1, @mp2].each do |micropost|
-        Micropost.find_by_id(micropost.id).should be_nil
-      end
-    end
+          @user = User.create(@attr)
+          @mp1 = Factory(:micropost, :user => @user, :created_at => 1.day.ago)
+          @mp2 = Factory(:micropost, :user => @user, :created_at => 1.hour.ago)
+        end
+
+        it "should have a microposts attribute" do
+          @user.should respond_to(:microposts)
+        end
+
+        it "should have the right microposts in the right order" do
+          @user.microposts.should == [@mp2, @mp1]
+        end
+
+        it "should destroy associated microposts" do
+          @user.destroy
+          [@mp1, @mp2].each do |micropost|
+            lambda do
+              Micropost.find(micropost)
+            end.should raise_error(ActiveRecord::RecordNotFound)
+          end
+        end
     
     describe "status feed" do
-
+        
       it "should have a feed" do
         @user.should respond_to(:feed)
       end
-
+        
       it "should include the user's microposts" do
-        @user.feed.include?(@mp1).should be_true
-        @user.feed.include?(@mp2).should be_true
+        @user.feed.should include(@mp1)
+        @user.feed.should include(@mp2)
       end
-
+        
       it "should not include a different user's microposts" do
-        mp3 = Factory(:micropost, 
+        mp3 = Factory(:micropost,
                       :user => Factory(:user, :email => Factory.next(:email)))
-        @user.feed.include?(mp3).should be_false
+        @user.feed.should_not include(mp3)
+      end
+        
+      it "should include the microposts of followed users" do
+        followed = Factory(:user, :email => Factory.next(:email))
+        mp3 = Factory(:micropost, :user => followed)
+        @user.follow!(followed)
+        @user.feed.should include(mp3)
       end
     end
   end
